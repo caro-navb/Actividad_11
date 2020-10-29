8744a3256789d0d36e34cdc20a3a195d88064043
Carolina Navarro <silvia.navarro7499@alumnos.udg.mx>
2020-10-29 12:56:53 -0600

    Agregando al repositorio algoritmos.py, particula.py y administrador_particulas.py

---
+++ administrador_particulas.py
@@ -0,0 +1,41 @@
+from particula import Particula
+import json
+
+class Administrador:
+    def __init__(self):
+        self.__particulas = []
+
+    def agregar_inicio(self, particula:Particula):
+        self.__particulas.insert(0, particula)
+
+    def agregar_final(self, particula:Particula):
+        self.__particulas.append(particula)
+
+    def mostrar(self):
+        for particula in self.__particulas:
+            print(particula)
+    
+    def __str__(self):
+        return "".join(
+            str(particula) + '\n' for particula in self.__particulas
+        ) 
+
+    def guardar(self, ubicacion):
+        try:
+            with open(ubicacion, 'w') as archivo:
+                lista = [particula.to_dict() for particula in self.__particulas]
+                print(lista)
+                json.dump(lista, archivo, indent=5)
+            return 1
+        except:
+            return 0
+
+    def abrir(self, ubicacion):
+        try:
+            with open(ubicacion, 'r') as archivo:
+                lista = json.load(archivo)
+                self.__particulas = [Particula(**particula) for paticula in lista]
+            return 1
+        except:
+            return 0
+
